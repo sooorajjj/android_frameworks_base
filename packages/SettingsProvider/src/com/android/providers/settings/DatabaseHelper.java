01a908af7ac6224fbc3a0d124d6bf67361ea6520
Andrew Chen <andrew.chen@gigabyte.com>
2013-04-13 02:30:13 +0800

    Allow default input method
    Category: input_method
    Bug: None
    
    Change-Id: I75c589915982cb3af21f61ff8b278f5a0e0a4447
    Signed-off-by: Andrew Chen <andrew.chen@gigabyte.com>

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -1713,6 +1713,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
             loadIntegerSetting(stmt, Settings.Secure.WIFI_MAX_DHCP_RETRY_COUNT,
                     R.integer.def_max_dhcp_retries);
+
+            loadSetting(stmt, Settings.Secure.DEFAULT_INPUT_METHOD,
+                    SystemProperties.get("ro.sf.def_input_method", mContext.getResources().getString(R.string.def_input_method))
+                    );
         } finally {
             if (stmt != null) stmt.close();
         }

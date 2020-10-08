sooorajjj/android_frameworks_base

commit 46f7a11019bc0ba4a737dc1873e0e97a83491504

Implement OpenGL readback via access to private HAL data
On Adreno 330, reading back a GL texture that is bound to an EGLImage
and native Android window buffer will lead to artifacts. Some rows in
the image data will occasionally be null.

To work around this issue, access the underlying buffer data directly
from GPU to CPU memory. For this to work, access to internal HAL data
structures is required; therefore this patch is highly
platform-specific.

This issue affects all CtsUiRenderingTestCases that use
Bitmap.Config.HARDWARE, testBitmapConfigFromHardwareToHardware being the
minimal example amongst them.

Issue: FP2P-425
Test: adb shell setprop hwui.private_hal_readback 1
Test: run cts -m CtsUiRenderingTestCases
  -t android.uirendering.cts.testclasses.HardwareBitmapTests#testBitmapConfigFromHardwareToHardware
Change-Id: I9aeb21b12367dea428253cdb9cc2319692e5f329

libs/hwui/OpenGLReadback.cpp
@@ -29,6 +29,13 @@
 #include <ui/Fence.h>
 #include <ui/GraphicBuffer.h>
 
+#include <cutils/properties.h>
+
+// Direct access to private HAL data to workaround Adreno 330 driver bugs.
+// DO NOT actually call anything from this header; it's included only to access
+// private structs.
+#include "../../../../hardware/qcom/display/libgralloc/gralloc_priv.h"
+
 namespace android {
 namespace uirenderer {
 
@@ -62,9 +69,109 @@ CopyResult OpenGLReadback::copySurfaceInto(Surface& surface, const Rect& srcRect
     return copyGraphicBufferInto(sourceBuffer.get(), texTransform, srcRect, bitmap);
 }
 
+namespace {
+
+CopyResult copyFromPrivateHandle(GraphicBuffer* graphicBuffer, Matrix4& /*texTransform*/,
+                                 const Rect& srcRect, SkBitmap* bitmap) {
+
+    static const bool workaround_enabled =
+        [] () -> bool {
+        const int value = property_get_bool("hwui.private_hal_readback", 0) == 1;
+        ALOGD("copyFromPrivateHandle: hwui.private_hal_readback=%i", value);
+        return bool(value);
+    }();
+
+    if (!workaround_enabled) {
+        return CopyResult::UnknownError;
+    }
+
+    if (bitmap->colorType() != kRGBA_8888_SkColorType) {
+        ALOGI("copyFromPrivateHandle: Only RGBA_8888 is supported.");
+        return CopyResult::SourceInvalid;
+    }
+    const size_t bytesPerPixel = bitmap->bytesPerPixel();
+
+    const native_handle_t* native_handle = graphicBuffer->handle;
+    const bool handleIsAsExpected = private_handle_t::validate(native_handle) == 0;
+    if (!handleIsAsExpected) {
+        ALOGE("copyFromPrivateHandle: GraphicBuffer doesn't seem to map to gralloc private handle.");
+        return CopyResult::SourceInvalid;
+    }
+    const private_handle_t* hnd = static_cast<const private_handle_t*>(native_handle);
+
+    const int bitmapWidth = bitmap->width();
+    const int bitmapHeight = bitmap->height();
+    // May be aligned and be larger than the actual image.
+    const int bufferWidth = hnd->width;
+    const int bufferHeight = hnd->height;
+    if (bitmapWidth > bufferWidth || bitmapHeight > bufferHeight) {
+        ALOGE("copyFromPrivateHandle: bitmap is larger than the buffer. This is not supposed to happen.");
+        return CopyResult::SourceInvalid;
+    }
+    const size_t bufferRowBytes = bufferWidth * bytesPerPixel;
+    // We access as many rows as the bitmap has, aligned to the width of the
+    // buffer.
+    const size_t minBufferSize = bufferRowBytes * bitmapHeight;
+    if (hnd->size < 0 || static_cast<size_t>(hnd->size) < minBufferSize) {
+        ALOGE("copyFromPrivateHandle: buffer is smaller than expected or invalid.");
+        return CopyResult::SourceInvalid;
+    }
+
+    const char* bufferData = reinterpret_cast<const char*>(hnd->base);
+
+    if (srcRect.isEmpty() && bitmapWidth == bufferWidth) {
+        // Take the quick path if possible
+        void* bitmapPixelAddr = bitmap->getPixels();
+        if (!bitmapPixelAddr) {
+            ALOGE("copyFromPrivateHandle: Bitmap pixel address is NULL");
+            return CopyResult::DestinationInvalid;
+        }
+        memcpy(bitmapPixelAddr, bufferData, static_cast<size_t>(hnd->size));
+    } else {
+        // The buffer has some extra space for alignment or we copy only a
+        // subset, or both. Copy requested pixels row-by-row.
+        int left = 0, top = 0;
+        if (!srcRect.isEmpty()) {
+            // Assuming that the floats in srcRect are actually integers.
+            left = static_cast<int>(srcRect.left);
+            top = static_cast<int>(srcRect.top);
+        }
+        // Check that the selected rect is within buffer range. Top/left corner
+        // is defined by srcRect, storage width and height by the bitmap, as
+        // done in equivalent GL implementation.
+        if (left + bitmapWidth > bufferWidth || top + bitmapHeight > bufferHeight) {
+            ALOGE("copyFromPrivateHandle: srcRect is larger than the buffer. This is not supposed to happen.");
+            return CopyResult::DestinationInvalid;
+        }
+        const size_t bitmapRowBytes = bitmapWidth * bytesPerPixel;
+        const char* bufferTop = bufferData + bufferRowBytes * top;
+        for (int y = 0; y < bitmapHeight; ++y) {
+            void* bitmapRowAddr = bitmap->getAddr(0, y);
+            if (!bitmapRowAddr) {
+                ALOGE("copyFromPrivateHandle: Bitmap address is NULL for row %i", y);
+                return CopyResult::DestinationInvalid;
+            }
+            const void* bufferRowAddr = bufferTop
+                + static_cast<size_t>(y) * bufferRowBytes
+                + static_cast<size_t>(left) * bytesPerPixel;
+            memcpy(bitmapRowAddr, bufferRowAddr, bitmapRowBytes);
+        }
+    }
+
+    bitmap->notifyPixelsChanged();
+
+    return CopyResult::Success;
+}
+
+} // anonymous namespace
+
 CopyResult OpenGLReadback::copyGraphicBufferInto(GraphicBuffer* graphicBuffer,
                                                  Matrix4& texTransform, const Rect& srcRect,
                                                  SkBitmap* bitmap) {
+    if (copyFromPrivateHandle(graphicBuffer, texTransform, srcRect, bitmap) == CopyResult::Success) {
+        return CopyResult::Success;
+    }
+
     mRenderThread.eglManager().initialize();
     // TODO: Can't use Image helper since it forces GL_TEXTURE_2D usage via
     // GL_OES_EGL_image, which doesn't work since we need samplerExternalOES

commit 9f98001400bd8ccbe087de91566d3af1f7b13a68

Check transforms params in EGLImage readback workaround
The workaround does currently not implement texture transformation,
neither via transformation matrix, nor via scaling defined via different
sizes of source rect, source GraphicBuffer and destination bitmap. Fall
back to the default implementation in all those cases.

Issue: FP2P-449
Fixes: 46f7a11019b (Implement OpenGL readback via access to private HAL data)
Test: run cts -m CtsViewTestCases -t android.view.cts.PixelCopyTest
Test: run cts -m CtsViewTestCases -t android.view.cts.TextureViewCameraTest
Change-Id: I6157fcd7cb9d93f94cb088ff1dc50dab760e766d

libs/hwui/OpenGLReadback.cpp
@@ -71,7 +71,7 @@ CopyResult OpenGLReadback::copySurfaceInto(Surface& surface, const Rect& srcRect
 
 namespace {
 
-CopyResult copyFromPrivateHandle(GraphicBuffer* graphicBuffer, Matrix4& /*texTransform*/,
+CopyResult copyFromPrivateHandle(GraphicBuffer* graphicBuffer, Matrix4& texTransform,
                                  const Rect& srcRect, SkBitmap* bitmap) {
 
     static const bool workaround_enabled =
@@ -82,6 +82,33 @@ CopyResult copyFromPrivateHandle(GraphicBuffer* graphicBuffer, Matrix4& /*texTra
     }();
 
     if (!workaround_enabled) {
+        return CopyResult::UnknownError;
+    }
+
+    static const Matrix4 defaultTransform = []() {
+        const float matrixData[] = {
+            1.f,  0.f,  0.f,  0.f,
+            0.f, -1.f,  0.f,  0.f,
+            0.f,  0.f,  1.f,  0.f,
+            0.f,  1.f,  0.f,  1.f
+        };
+        return Matrix4(matrixData);
+    }();
+
+    const int bitmapWidth = bitmap->width();
+    const int bitmapHeight = bitmap->height();
+
+    if (static_cast<int64_t>(graphicBuffer->getWidth()) != bitmapWidth
+        || static_cast<int64_t>(graphicBuffer->getHeight()) != bitmapHeight
+        || (!srcRect.isEmpty() &&
+            (srcRect.getWidth() != bitmapWidth || srcRect.getHeight() != bitmapHeight))
+        || texTransform != defaultTransform) {
+        // Image transformation isn't supported in any way here. Fall back to
+        // the default implementation.
+        // See CtsViewTestCases/android.view.cts.PixelCopyTest for the various
+        // ways in which scaling needs to be supported.
+        ALOGI("copyFromPrivateHandle: Image transformation is requested but not supported. "
+            "Falling back to the default implementation.");
         return CopyResult::UnknownError;
     }
 
@@ -99,8 +126,6 @@ CopyResult copyFromPrivateHandle(GraphicBuffer* graphicBuffer, Matrix4& /*texTra
     }
     const private_handle_t* hnd = static_cast<const private_handle_t*>(native_handle);
 
-    const int bitmapWidth = bitmap->width();
-    const int bitmapHeight = bitmap->height();
     // May be aligned and be larger than the actual image.
     const int bufferWidth = hnd->width;
     const int bufferHeight = hnd->height;